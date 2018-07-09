8e1a124839019f4aa88c156d356aa9a8e31f0020
Pawan Chhabria <chhabriapawan01@gmail.com>
2018-07-09 21:58:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+app.get('/',(req,res)=>{
+
+  res.send("Hello Express!");
+} );
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+        pageTitle:'About Page'
+
+});
+
+});
+
+app.listen(3000);
